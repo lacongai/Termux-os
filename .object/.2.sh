6c53efd397a2d156553aa9b682a97b1696d7529e
HenTaiz <thailanfreefire2000@gmail.com>
2026-06-13 17:30:57 +0700

    Update .2.sh

--- .object/.2.sh
+++ .object/.2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 DAT(){ echo -en "\033[${1};${2}H";}
-clear;toilet -t -f mono12  "TERMUX-MOD" --gay -F border
+clear;toilet -t -f mono12  "TERMUX" --gay -F border
 echo ""
 DAT 11 40
 echo -e "\e[92mBởi\e[1;93m Gấu Ngốc Nghếch\e[1;95m /\e[1;96m @henntaiiz "
